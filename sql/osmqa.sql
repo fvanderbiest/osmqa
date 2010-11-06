f9072000e9c709138aa4b25f1bec148a7a11a044
fvanderbiest <francois.vanderbiest@gmail.com>
2010-11-06 11:15:21 +0000

    missing file in r95

--- sql/osmqa.sql
+++ sql/osmqa.sql
@@ -4,7 +4,7 @@
 CREATE TABLE maps(
     id serial primary key,
     name varchar(255), -- the following vars are not yet useful
-    "left" float, -- map's bbox in epsg:900913
+    "left" float, -- map bbox in epsg:900913
     "bottom" float,
     "right" float,
     "top" float,
@@ -53,7 +53,7 @@ CREATE TABLE tags(
     name boolean default false,
     "ref" boolean default false,
     place boolean default false,
-    address boolean default false,
+    addr boolean default false,
     primary key (map_id, tile_geometry_id)
 );
 CREATE INDEX tags_map_id_idx ON tags (map_id); 
@@ -63,14 +63,14 @@ ALTER TABLE tags ADD CONSTRAINT fk_tags_map_id FOREIGN KEY (map_id) REFERENCES m
 
 -- DROP VIEW tiles;
 CREATE VIEW tiles AS 
-SELECT t.tile_geometry_id as id, t.reserved, t.highway, t.building, t.landuse, g.geometry  
+SELECT t.tile_geometry_id as id, t.reserved, t.highway, t.building, t.landuse, t.barrier, t.cycleway, t.tracktype, t.waterway, t.railway, t.aeroway, t.aerialway, t.power, t.man_made, t.leisure, t.amenity, t.office, t.shop, t.tourism, t.historic, t.military, t."natural", t.sport, t.abutters, t.name, t."ref", t.place, t.addr, g.geometry  
 FROM tags t 
 LEFT JOIN tile_geometries g 
 ON t.tile_geometry_id = g.id;
 --WHERE map_id = 1;
 
 CREATE OR REPLACE RULE update_tiles AS ON UPDATE TO tiles DO INSTEAD (
-  UPDATE tags SET updated_at = now(), reserved = NEW.reserved, highway = NEW.highway, building = NEW.building, landuse = NEW.landuse WHERE tile_geometry_id = NEW.id
+  UPDATE tags SET updated_at = now(), reserved = NEW.reserved, highway = NEW.highway, building = NEW.building, landuse = NEW.landuse, barrier = NEW.barrier, cycleway = NEW.cycleway, tracktype = NEW.tracktype, waterway = NEW.waterway, railway = NEW.railway, aeroway = NEW.aeroway, aerialway = NEW.aerialway, power = NEW.power, man_made = NEW.man_made, leisure = NEW.leisure, amenity = NEW.amenity, office = NEW.office, shop = NEW.shop, tourism = NEW.tourism, historic = NEW.historic, military = NEW.military, "natural" = NEW."natural", sport = NEW.sport, abutters = NEW.abutters, name = NEW.name, "ref" = NEW."ref", place = NEW.place, addr = NEW.addr WHERE tile_geometry_id = NEW.id
 );
 CREATE OR REPLACE RULE delete_tiles AS ON DELETE TO tiles DO INSTEAD ();
 CREATE OR REPLACE RULE insert_tiles AS ON INSERT TO tiles DO INSTEAD ();
